dd1d17024336047efd2a68d79d1496f06b448123
Simon Kuenzer <simon.kuenzer@neclab.eu>
2020-10-02 10:46:18 +0200

    Copy code from MiniCache
    The code was copied from MiniCache @ 5a8ff7f.
    
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

---
+++ htable.c
@@ -0,0 +1,171 @@
+/*
+ * Simple hash table implementation
+ *
+ * Authors: Simon Kuenzer <simon.kuenzer@neclab.eu>
+ *
+ *
+ * Copyright (c) 2013-2017, NEC Europe Ltd., NEC Corporation All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, or the BSD license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#if defined(__MINIOS__)
+#include <target/sys.h>
+#include <mini-os/os.h>
+#include <mini-os/types.h>
+#include <mini-os/xmalloc.h>
+#elif defined(__KERNEL__)
+#include <target/sys.h>
+#include <linux/types.h>
+#include <target/stubs.h>
+#include <asm/bug.h>
+#else
+#include <stdlib.h>
+#include <string.h>
+
+#define target_free(p) free(p)
+#define target_malloc(a, l) malloc(l)
+#endif
+
+#ifndef MIN_ALIGN
+#define MIN_ALIGN ((size_t) 8)
+#endif
+
+#include "htable.h"
+
+#ifndef max
+#define max(a, b) \
+    ({ __typeof__ (a) __a = (a); \
+       __typeof__ (b) __b = (b); \
+       __a > __b ? __a : __b; })
+#endif
+
+static inline size_t align_up(size_t size, size_t align)
+{
+  return (size + align - 1) & ~(align - 1);
+}
+
+struct htable *alloc_htable(uint32_t nb_bkts, uint32_t el_per_bkt, uint8_t hlen, size_t el_private_len, size_t align)
+{
+	size_t ht_size;
+	size_t bkt_hdr_size;
+	size_t bkt_size;
+	size_t el_hdr_size;
+	size_t el_size;
+	struct htable *ht;
+	struct htable_bkt *bkt;
+	struct htable_el *el;
+	uint32_t i, j;
+
+	align = max(MIN_ALIGN, align);
+
+	el_hdr_size  = align_up(sizeof(struct htable_el), align);
+	el_size      = el_hdr_size + align_up(el_private_len, align);
+	bkt_hdr_size = align_up(sizeof(struct htable_bkt)
+	               + (sizeof(hash512_t) * el_per_bkt), align); /* hash list */
+	bkt_size     = bkt_hdr_size
+		       + (el_size * el_per_bkt) /* element list */;
+	ht_size      = sizeof(struct htable)
+		       + sizeof(struct htable_bkt *) * nb_bkts;
+
+#ifdef HTABLE_DEBUG
+	printf("el_size  = %lu B\n", el_size);
+	printf("bkt_size = %lu B\n", bkt_size);
+	printf("ht_size  = %lu B\n", ht_size);
+#endif
+	/* allocate main htable struct */
+	ht = target_malloc(align, ht_size);
+	if (!ht) {
+		errno = ENOMEM;
+		goto err_out;
+	}
+	memset(ht, 0, ht_size);
+
+#ifdef HTABLE_DEBUG
+	printf("htable (%lu B) @ %p\n", ht_size);
+#endif
+	ht->nb_bkts = nb_bkts;
+	ht->el_per_bkt = el_per_bkt;
+	ht->hlen = hlen;
+	ht->head = NULL;
+	ht->tail = NULL;
+
+	/* allocate buckets */
+	for (i = 0; i < nb_bkts; ++i) {
+		bkt = target_malloc(align, bkt_size);
+		if (!bkt) {
+			errno = ENOMEM;
+			goto err_free_bkts;
+		}
+		memset(bkt, 0, bkt_size);
+
+#ifdef HTABLE_DEBUG
+		printf(" bucket %lu (%lu B) @ %p\n", i, bkt_size, bkt);
+#endif
+		ht->b[i] = bkt;
+		bkt->el = (void *) (((uint8_t *) ht->b[i]) + bkt_hdr_size);
+		bkt->el_size = el_size;
+		bkt->el_private_len = el_private_len;
+
+		for (j = 0; j < el_per_bkt; ++j) {
+			el = _htable_bkt_el(bkt, j);
+			el->h = &bkt->h[j];
+			el->private = (void *) (((uint8_t *) el) + el_hdr_size);
+
+#ifdef HTABLE_DEBUG
+			//printf("  entry %3lu:%02lu (%p): h = @%p; private = @%p\n",
+			//        i, j, el, el->h, el->private);
+#endif
+		}
+	}
+
+	return ht;
+
+ err_free_bkts:
+	for (i = 0; i < nb_bkts; ++i) {
+		if (ht->b[i]) {
+			target_free(ht->b[i]);
+		}
+	}
+	target_free(ht);
+ err_out:
+	return NULL;
+}
+
+void free_htable(struct htable *ht)
+{
+	uint32_t i;
+
+	for (i = 0; i < ht->nb_bkts; ++i) {
+		if (ht->b[i]) {
+			target_free(ht->b[i]);
+		}
+	}
+	target_free(ht);
+}
---
+++ tools_common.h
@@ -0,0 +1,150 @@
+/*
+ * Simple hash filesystem (SHFS) tools
+ *
+ * Authors: Simon Kuenzer <simon.kuenzer@neclab.eu>
+ *
+ *
+ * Copyright (c) 2013-2017, NEC Europe Ltd., NEC Corporation All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
+#ifndef _TOOLS_COMMON_
+#define _TOOLS_COMMON_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <inttypes.h>
+
+#include "shfs_defs.h"
+
+/*
+ * Print helpers
+ */
+extern unsigned int verbosity;
+extern int force;
+
+#define eprintf(...)		fprintf(stderr, __VA_ARGS__)
+#define fatal()			eprintf("%s\n", strerror(errno))
+#define dief(...)		do { eprintf(__VA_ARGS__); exit(EXIT_FAILURE); } while(0)
+#define die()			do { fatal(); exit(EXIT_FAILURE); } while(0)
+#define dprintf(LEVEL, ...)	do { if (verbosity >= (LEVEL)) fprintf(stderr, __VA_ARGS__); } while(0)
+#define printvar(VAR, FMT)	do { if (verbosity >= (D_MAX)) fprintf(stderr, #VAR ": "FMT"\n", (VAR)); } while(0)
+
+#define D_L0		1
+#define D_L1		2
+#define D_MAX		D_L1
+
+
+/*
+ * Argument parsing helper
+ */
+static inline int parse_args_setval_str(char** out, const char* buf)
+{
+	if (*out)
+		free(*out);
+	*out = strdup(buf);
+	if (!*out) {
+		*out = NULL;
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+static inline int parse_args_setval_int(int* out, const char* buf)
+{
+	if (sscanf(optarg, "%d", out) != 1)
+		return -EINVAL;
+	return 0;
+}
+
+/*
+ * Disk I/O
+ */
+struct disk {
+	int fd;
+	char *path;
+	uint64_t size;
+	uint32_t blksize;
+	int discard;
+};
+
+struct disk *open_disk(const char *path, int mode);
+void close_disk(struct disk *d);
+
+struct vol_member {
+	struct disk *d;
+	uuid_t uuid;
+};
+
+struct storage {
+	struct vol_member member[SHFS_MAX_NB_MEMBERS];
+	uint8_t nb_members;
+	uint32_t stripesize;
+	uint8_t stripemode;
+};
+
+int sync_io_chunk(struct storage *s, chk_t start, chk_t len, int owrite, void *buffer);
+#define sync_read_chunk(s, start, len, buffer)	  \
+	sync_io_chunk((s), (start), (len), 0, (buffer))
+#define sync_write_chunk(s, start, len, buffer)	  \
+	sync_io_chunk((s), (start), (len), 1, (buffer))
+int sync_erase_chunk(struct storage *s, chk_t start, chk_t len);
+
+
+/*
+ * Misc
+ */
+void print_shfs_hdr_summary(struct shfs_hdr_common *hdr_common,
+                            struct shfs_hdr_config *hdr_config);
+chk_t metadata_size(struct shfs_hdr_common *hdr_common,
+                    struct shfs_hdr_config *hdr_config);
+chk_t avail_space(struct shfs_hdr_common *hdr_common,
+                  struct shfs_hdr_config *hdr_config);
+static inline void hash_unparse(hash512_t h, uint8_t hlen, char *out)
+{
+	uint8_t i;
+
+	for (i = 0; i < hlen; i++)
+		snprintf(out + (2*i), 3, "%02x", h[i]);
+}
+
+static inline size_t strftimestamp_s(char *s, size_t slen, const char *fmt, uint64_t ts_sec)
+{
+	struct tm *tm;
+	time_t *tsec = (time_t *) &ts_sec;
+	tm = localtime(tsec);
+	return strftime(s, slen, fmt, tm);
+}
+
+size_t strshfshost(char *s, size_t slen, struct shfs_host *h);
+
+#endif /* _TOOLS_COMMON_ */
